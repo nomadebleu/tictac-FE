a6d3a8dba0692f1da8537a0ac73cbdb805670dda
Anthony <anthonyiezzi64@gmail.com>
2023-11-07 16:33:03 +0100

    script.js modifié

--- script.js
+++ script.js
@@ -1,29 +1,54 @@
-document.querySelector('#addBook').addEventListener('click', function () {
-	const tripBook = document.querySelector('#tripBookInput').value;
+//--------------Création des trajets dans book-container
 
-	fetch('http://localhost:3000/cart', {
-		method: 'POST',
-		headers: { 'Content-Type': 'application/json' },
-		body: JSON.stringify({ cityName }),
-	}).then(response => response.json())
+let bookings = [];//C'est le panier des trajet
+let total;//Montant total des trajets
+
+document.querySelector('#btn-search').addEventListener('click', function () {
+	fetch('http://localhost:3000/index/mycart')
+		.then (response => response.json())
 		.then(data => {
 			if (data.result) {
-				document.querySelector('#cityList').innerHTML += `
-			<div class="bookContainer">
-				<p class="departure">${data.weather.cityName}</p>
-				<p class="arrival">${data.weather.description}</p>
-				<img class="weatherIcon" src="images/${data.weather.main}.png"/>
-				<div class="temperature">
-					<p class="tempMin">${data.weather.tempMin}°C</p>
-					<span>-</span>
-					<p class="tempMax">${data.weather.tempMax}°C</p>
-				</div>
-				<button class="deleteCity" id="${data.weather.cityName}">Delete</button>
-			</div>
-					`;
-				updateDeleteCityEventListener();
-				document.querySelector('#cityNameInput').value = '';
+				Trip.forEach((trajet) => {
+					//Récupération de l'heure dans la clé date de l'objet trajet
+					const dateValue = trajet.date;
+					const timeValue = moment(dateValue).format('HH:mm');
+					//Création de chaque trajet avec les éléments
+					document.querySelector('#book-container').innerHTML += `
+					<div class ="trip">
+					<div>
+					  <p id="departure">${trajet.departure}</p>
+					</div>
+					<div>
+					  <p>></p>
+					</div>
+					<div>
+					  <p id="arrival">${trajet.arrival}</p>
+					</div>
+					<div>
+					  <p id="time">${timeValue}</p>
+					</div>
+					<div>
+					<p id="price">${trajet.price}</p>
+					</div>
+					<button id="book">BOOK</button>
+						`;
+					bookings.push(trajet);
+					//on fait le total des prix des différents trajets avec la méthode reduce((acc,e)=>{},départ)
+					total = bookings.reduce((cumul, trajet) => cumul + trajet.price, 0);
+				})
+			}else {
+				console.log('Pas de trajet')
 			}
-
-		});
+		})
 });
+
+//--------------Supprimer un TRAJET de son cart
+//Sur chaque trajet, j'ajoute une écoute.Au clic, cela l'enlève et met à jour le total des bookings
+for (let i = 0; i < document.querySelectorAll('.deleteTrip').length; i++) {
+	document.querySelectorAll('.deleteTrip')[i].addEventListener('click', function () {
+	this.parentNode.remove();
+	bookings = document.querySelectorAll('.trip')
+	total -=  
+	});
+}
+	
