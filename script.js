10b773f941e868fc8324cf4977bc1ed34a7936da
Anthony <anthonyiezzi64@gmail.com>
2023-11-08 20:54:23 +0100

    la connection avec la route fonctionne et cree les blocs voyage trouves GET>POST

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
+//console.log(moment(new Date()).format('YYYY-MM-DD'))
 //--------------Création des trajets dans BOOK-CONTAINER sur l'ACCUEIL
 
 let bookings = [];//C'est le panier des trajets
 let total;//Montant total des trajets
 
 document.querySelector('#btn-search').addEventListener('click', function () {
-	fetch('http://localhost:3000/mycart')
+	fetch('http://localhost:3000/mycart',{
+		method:'POST',
+		headers:{'Content-Type':'application/json'},
+		body:JSON.stringify({
+			departure:document.querySelector('#cityNameInputD').value,
+			arrival:document.querySelector('#cityNameInputA').value,
+			date:moment(document.querySelector('#travelDay').value).format('YYYY-MM-DD')
+		})
+	})
 		.then (response => response.json())
 		.then(data => {
 			console.log(data)
 			if (data.result) {
-				data.forEach((trajet) => {
+				data.trip.forEach((trajet) => {
 					//Récupération de l'heure dans la clé date de l'objet trajet
 					const dateValue = trajet.date;
 					const timeValue = moment(dateValue).format('HH:mm');
+					console.log(timeValue)
 					//Création de chaque trajet avec les éléments
 					document.querySelector('#book-container').innerHTML += `
 					<div class ="trip">
@@ -34,11 +44,12 @@ document.querySelector('#btn-search').addEventListener('click', function () {
 					<button type="button" class="book btn btn-success">BOOK</button>
 					<button type="button" id="delete" class="btn btn-success">X</button>
 						`;
-					bookings.push(trajet);
+					console.log(bookings.push(trajet));
 					//on fait le total des prix des différents trajets avec la méthode reduce((acc,e)=>{},départ)
 					total = bookings.reduce((cumul, trajet) => cumul + trajet.price, 0);
+					console.log(total)
 				})
-				document.querySelector('#book-container').innerHTML = '';
+				
 			}else {
 				console.log('Pas de trajet')
 			}
@@ -55,7 +66,7 @@ for (let i = 0; i < document.querySelectorAll('.deleteTrip').length; i++) {
 });
 }
 //--------------Calcul du temps restant avant le départ
-
+/*
 //Il faut récupérer ou connecter timeD avec les données
 const timeD = "2023-11-08T06:27:46.071Z"
 //Conversion pour récuperer l'heure
@@ -71,3 +82,4 @@ const dureeHeures = Math.floor(duree / 60);
 const dureeMinutes = duree % 60;
 //Il faut injecter le résultat dans le FE
 console.log(`Departure in ${dureeHeures}h ${dureeMinutes}min.`);
+*/
