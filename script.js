6223f7f50e75b5650b479f1b47d92c986b8da70a
Anthony <anthonyiezzi64@gmail.com>
2023-11-09 16:33:45 +0100

    script pb pour afficher le bloc dans cart.html

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-//RECHERCHE DES TRAJETS------------------------------------------------------------------>
-
+//Recherche des trajets ------------------------------------------------------------->
 let bookings = [];//C'est le panier des trajets
 let total;//Montant total des trajets
 let searchData;//On sort les data pour les réutiliser ensuite
@@ -16,6 +15,7 @@ document.querySelector('#btn-search').addEventListener('click', function () {
     })
         .then (response => response.json())
         .then(data => {
+            console.log(data)
             searchData = data;
             if (data.result) {
                 // Rajout de l'index pour identifier chaque trajet
@@ -44,14 +44,16 @@ document.querySelector('#btn-search').addEventListener('click', function () {
                             <button type="button" class="book btn btn-success">BOOK</button>
                         </div>
                     `;
+                    console.log('Contenu de #book-container après la modification :', document.querySelector('#book-container').innerHTML);
                     // RETRAIT des IMAGES & MESS avec DISPLAY NONE
                     document.querySelector('.img-content-right').style.display = 'none';
+                    //Push le trajet pour calculer ensuite le prix total
                     bookings.push(trajet)
                     // TOTAL l'ensemble des TRAJETS trouvés avec .reduce((acc,e)=>{},départ)
                     total = bookings.reduce((cumul, trajet) => cumul + trajet.price, 0);
                 });
 
-                // Appel de la fonction pour ajouter les écouteurs d'événements après avoir créé les trajets
+                // Fonction pour ajouter des écouteurs à chaque trajet une fois qu'ils sont créés
                 addEventListenersToBookButtons();
             } else {
                 console.log('Pas de trajet')
@@ -59,51 +61,75 @@ document.querySelector('#btn-search').addEventListener('click', function () {
         })
 });
 
-// FONCTION pour ajouter les écouteurs d'événements aux boutons "BOOK"
+// Fonction pour ajouter les écouteurs au bouton BOOK de chaque trajet
 function addEventListenersToBookButtons() {
-    const trajetFound = document.querySelectorAll('.trip');
-    trajetFound.forEach((trajet, index) => {
-        trajet.querySelector('.btn-success').addEventListener('click', function () {
+    const bookButtons = document.querySelectorAll('.btn-success');
+
+    bookButtons.forEach((button, index) => {
+        button.addEventListener('click', function () {
             const selectedTrajet = searchData.trip[index];
-            bookings.push(selectedTrajet);
-            trajet.remove();
-            // Redirigez vers cart.html avec les détails du trajet
-            window.location.href = `cart.html?departure=${selectedTrajet.departure}&arrival=${selectedTrajet.arrival}&time=${selectedTrajet.time}&price=${selectedTrajet.price}`;
+            console.log(selectedTrajet);
+
+            // Conversion de la date
+            const dateObject = new Date(selectedTrajet.date);
+
+            // Utilisation de toISOString pour formater la date pour l'intégrer à l'URL
+            const timeValue = dateObject.toISOString();
+            console.log(timeValue);
+
+            button.parentNode.remove();
+            window.location.href = `cart.html?departure=${selectedTrajet.departure}&arrival=${selectedTrajet.arrival}&time=${timeValue}&price=${selectedTrajet.price}`;
         });
     });
 }
-	    
-		// //CART:INJECTION TRIP avec DELETE
-		// document.querySelector('#book-container-cart').innerHTML += `
-		// 	<div class ="trip">
-		// 		<div>
-		// 			<p id="departure">${trajet.departure}</p>
-		// 		</div>
-		// 		<div>
-		// 			<p>></p>
-		// 		</div>
-		// 		<div>
-		// 			<p id="arrival">${trajet.arrival}</p>
-		// 		</div>
-		// 		<div>
-		// 		<p id="time">${timeValue}</p>
-		// 		</div>
-		// 		<div>
-		// 			<p id="price">${trajet.price}€</p>
-		// 		</div>
-	  	// 		<button type="button" id="delete" class="btn btn-success">X</button>
-		// 	</div>
-		// `
 
-		// //CART:INJECTION du TITRE & CHECKOUT
-		// document.querySelector('#result-content-cart').innerHTML += `
-		// 	<span class="title-result-content">My Cart</span>
-			
-		// 	<div id="checkout-summary">
-        // 		<p id="total-title">Total:<span id="total-amount">103€</span></p>
-        // 		<button type="button" id="purchase" class="btn btn-success">Purchase</button>
-    	// 	</div>
-		// `
+// Récupération des données dans cart.html
+
+const urlParams = new URLSearchParams(window.location.search);
+const departure = urlParams.get('departure');
+const arrival = urlParams.get('arrival');
+const time = urlParams.get('time');
+const price = urlParams.get('price');
+
+// CART Affiche le trajet avec DELETE
+document.querySelector('#book-container-cart').innerHTML += `
+    <div class="trip">
+        <div>
+            <p id="departure">k</p>
+        </div>
+        <div>
+            <p>></p>
+        </div>
+        <div>
+            <p id="arrival">k</p>
+        </div>
+        <div>
+            <p id="time">k</p>
+        </div>
+        <div>
+            <p id="price">k€</p>
+        </div>
+        <button type="button" id="delete" class="btn btn-success">X</button>
+    </div>
+`;
+    
+
+
+    //CART:INJECTION du TITRE & CHECKOUT
+    document.querySelector('#result-content-cart').innerHTML += `
+        <span class="title-result-content">My Cart</span>
+        
+        <div id="checkout-summary">
+            <p id="total-title">Total:<span id="total-amount">103€</span></p>
+            <button type="button" id="purchase" class="btn btn-success">Purchase</button>
+        </div>
+    `;
+
+	    
+		
+		
+
+		
 	
    
 
@@ -139,11 +165,6 @@ function addEventListenersToBookButtons() {
 // document.querySelector('#result-content-booking').innerHTML += `
 // 	<span class="title-result-content">My Bookings</span>
 // `
-
-	
-
-
-
 
 
 //--------------Supprimer un TRAJET de son CART
