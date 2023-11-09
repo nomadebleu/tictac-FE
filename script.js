b492a23c12c448550dcfa2b21f999f2baed906d1
Anthony <anthonyiezzi64@gmail.com>
2023-11-09 10:25:18 +0100

    restructuration html ds scrip js

--- script.js
+++ script.js
@@ -49,6 +49,7 @@ document.querySelector('#btn-search').addEventListener('click', function () {
 					console.log(total)
 				})
 			}else {
+			
 				console.log('Pas de trajet')
 			}
 		})
@@ -58,6 +59,7 @@ document.querySelector('#btn-search').addEventListener('click', function () {
 for (let i=0; i < document.querySelectorAll('.trip').length; i++) {
 	document.querySelectorAll('.trip')[i].addEventListener('click', function(){
 	  this.parentNode.remove();
+	  //Bloc voyage avec bouton DELETE & SS BOOK
 	  document.querySelector('#book-container-cart').innerHTML += `
 					<div class ="trip">
 					<div>
@@ -83,9 +85,66 @@ for (let i=0; i < document.querySelectorAll('.trip').length; i++) {
    
 
 
-
-
-
+//BOOKING injection VOYAGE avec DUREE avant départ & FOOTER
+document.querySelector('#book-container-cart').innerHTML += `
+	<div class ="trip">
+		<div>
+  			<p id="departure">${trajet.departure}</p>
+		</div>
+		<div>
+  			<p>></p>
+		</div>
+		<div>
+  			<p id="arrival">${trajet.arrival}</p>
+		</div>
+		<div>
+  			<p id="time">${timeValue}</p>
+		</div>
+		<div>
+			<p id="price">${trajet.price}€</p>
+		</div>
+		<div>
+			<p id="duree">Departure in 5 hours</p>
+		</div>
+		<div id="bloc-footer">
+			<hr class="line" style="width: 40%" />
+    		<p class="pub">Enjoy your travels with Tickethack</p>
+		</div>
+	</div>
+`
+//BOOKING injection du TITRE
+document.querySelector('#result-content-booking').innerHTML += `
+	<span class="title-result-content">My Bookings</span>
+`
+//CART injection VOYAGE avec DELETE & CHECKOUT
+document.querySelector('#book-container-cart').innerHTML += `
+	<div class ="trip">
+		<div>
+			<p id="departure">${trajet.departure}</p>
+		</div>
+		<div>
+			<p>></p>
+		</div>
+		<div>
+			<p id="arrival">${trajet.arrival}</p>
+		</div>
+		<div>
+			<p id="time">${timeValue}</p>
+		</div>
+		<div>
+			<p id="price">${trajet.price}€</p>
+		</div>
+	  	<button type="button" id="delete" class="btn btn-success">X</button>
+	</div>
+	<div id="checkout-summary">
+        <p id="total-title">Total:<span id="total-amount">103€</span></p>
+        <button type="button" id="purchase" class="btn btn-success">Purchase</button>
+    </div>
+`
+//CART injection du TITRE
+document.querySelector('#result-content-cart').innerHTML += `
+	<span class="title-result-content">My Cart</span>
+`
 
 
 
