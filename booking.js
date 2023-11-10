4b087a3fcd97ba5bd873e76ee95902034544a133
Anthony <anthonyiezzi64@gmail.com>
2023-11-10 11:53:15 +0100

    booking.js presque fini reste css

--- booking.js
+++ booking.js
@@ -1,49 +1,66 @@
-//BOOKING injection VOYAGE avec DUREE avant départ & FOOTER
+document.querySelector('#anyvoyage-booking').style.display = "none"
 document.querySelector('#book-container-booking').innerHTML += `
-	<div class ="trip">
-		<div>
-  			<p id="departure">${trajet.departure}</p>
-		</div>
-		<div>
-  			<p>></p>
-		</div>
-		<div>
-  			<p id="arrival">${trajet.arrival}</p>
-		</div>
-		<div>
-  			<p id="time">${timeValue}</p>
-		</div>
-		<div>
-			<p id="price">${trajet.price}€</p>
-		</div>
-		<div>
-			<p id="duree">Departure in 5 hours</p>
-		</div>
-		<div id="bloc-footer">
-			<hr class="line" style="width: 40%" />
-    		<p class="pub">Enjoy your travels with Tickethack</p>
-		</div>
-	</div>
-`
-//BOOKING injection du TITRE
-document.querySelector('#result-content-booking').innerHTML += `
 	<span class="title-result-content">My Bookings</span>
-`
+	`
+fetch("http://localhost:3000/mybookings")
+	.then(response => response.json())
+	.then(data => {
+		console.log(data)
+		data.allBookings.forEach(booking => {
 
-//--------------Calcul du temps restant avant le départ
+			//Conversion de 'time'
+			const dateValue = booking.time;
+			const timeValue = moment(dateValue).format("HH:mm");
 
-//Il faut récupérer ou connecter timeD avec les données
-const timeD = "2023-11-08T06:27:46.071Z"
-//Conversion pour récuperer l'heure
-const timeDepart = moment(timeD).format('HH:mm')
+			//Calcul durée le départ
+			//Conversion pour récuperer l'heure
+			const timeDepart = moment(dateValue).format('HH:mm')
 
-//Génération de la date actuelle avec conversion pour avoir l'heure
-const dateNew = new Date();
-const dateActuelle = moment(dateNew).format('HH:mm')
+			//Génération de la date actuelle avec conversion pour avoir l'heure
+			const dateNew = new Date();
+			const dateActuelle = moment(dateNew).format('HH:mm')
 
-//Calcul de la durée avec moment & conversion des min.en H+m
-const duree = moment(dateNew).diff(moment(timeD), 'minutes');
-const dureeHeures = Math.floor(duree / 60);
-const dureeMinutes = duree % 60;
-//Il faut injecter le résultat dans le FE
-console.log(`Departure in ${dureeHeures}h ${dureeMinutes}min.`);
+			//Calcul de la durée avec moment & conversion des min.en H+m
+			const duree = moment(dateNew).diff(moment(dateValue), 'minutes');
+			const dureeHeures = Math.floor(duree / 60);
+			const dureeMinutes = duree % 60;
+
+			//Création des bookings
+			document.querySelector('#book-container-booking').innerHTML += `
+				<div class ="trip">
+					<div>
+				  		<p id="departure">${booking.departure}</p>
+					</div>
+					<div>
+				  		<p>></p>
+					</div>
+					<div>
+				 		<p id="arrival">${booking.arrival}</p>
+					</div>
+					<div>
+				  		<p id="time">${timeValue}</p>
+					</div>
+					<div>
+						<p id="price">${booking.price}€</p>
+					</div>
+					<div>
+						<p id="duree">Departure in ${dureeHeures}h ${dureeMinutes}min.</p>
+					</div>
+				</div>
+			`
+	});
+			document.querySelector('#book-container-booking').innerHTML += `
+							
+					<div id="bloc-footer">
+						<hr class="line" style="width: 40%" />
+						<p class="pub">Enjoy your travels with Tickethack</p>
+					</div>
+			`
+});
+
+
+
+
+
+
+
